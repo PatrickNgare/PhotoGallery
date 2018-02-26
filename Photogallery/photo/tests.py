40f1f3347589390817f94bc446a365ff5c48d77b
patrickNgare <patrickngare@gmail.com>
2018-02-26 22:38:43 +0300

    added location test

--- Photogallery/photo/tests.py
+++ Photogallery/photo/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from . models import Gallery,Category,Image
 
-# Create your tests here.
+
+#create your tests here
+ class LocationTestClass(TestCase):
+    def setUp(self):
+         self.test_place(place="kirinyaga")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_place,Location))
+
+    def test_saving_place(self):
+        self.test_place.save_places()
+        place = Location.objects.all()
+        self.assertTrue(len(place) > 0)     
+
